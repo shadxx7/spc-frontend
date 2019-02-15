e8ba81a7fbc2ad2e1ba1ee32749956dfc234f619
Mohamed Shadab <shadabdragon@gmail.com>
2019-02-15 12:13:07 +0530

    Cleanup

--- src/containers/signin/index.js
+++ src/containers/signin/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import { Form, Icon, Input, Button, Alert, message } from "antd";
 
 //Action Creators
